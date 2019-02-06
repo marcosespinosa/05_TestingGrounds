1d73014fc7a1c28a9af5ddeea0d0e9a017876268
marcosespinosa22 <ing.marcosespinosa@gmail.com>
2019-02-06 12:14:18 -0800

    Changed color on guard's material. Patrol points is an array from c++ now.

--- Source/S05_TestingGrounds/ChooseNextWaypoint.h
+++ Source/S05_TestingGrounds/ChooseNextWaypoint.h
@@ -13,5 +13,13 @@ UCLASS()
 class S05_TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 {
 	GENERATED_BODY()
+
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector IndexKey;
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector WaypointKey;
 };
